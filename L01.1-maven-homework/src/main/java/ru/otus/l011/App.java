042621a92466974fc57dc150fb9bbb1dc103d91f
mikhail <goodlost@yandex.ru>
2018-02-04 08:54:22 +0300

    proguard added

--- L01.1-maven-homework/src/main/java/ru/otus/l011/App.java
+++ L01.1-maven-homework/src/main/java/ru/otus/l011/App.java
@@ -1,16 +1,20 @@
 package ru.otus.l011;
 
-import com.google.common.collect.Lists;
+
+import org.apache.commons.collections4.ListUtils;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 public class App {
 
-    public static void main(String... args) {
-        List<String> list = Lists.asList("Arguments:", args);
-        for (String aList : list) {
-            System.out.println(aList);
+    public static void main(String[] args) {
+        List<String> arguments = Arrays.asList(args);
+        List<String> firstList = Arrays.asList("Command Line","Arguments");
+        List<String> finalArguments = ListUtils.union(firstList,arguments);
+        for (String current:finalArguments){
+            System.out.println(current);
         }
     }
 }
